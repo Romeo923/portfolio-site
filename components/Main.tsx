8535490902bfcb1aeb76187b0820e2057d4ce64f
Romeo923 <cromeo@gmail.com>
2024-08-06 20:49:12 -0400

    updated main page

--- components/Main.tsx
+++ components/Main.tsx
@@ -12,16 +12,27 @@ import { fadeIn } from "@/utils/variants"
 
 export default function Main() {
 	const [job, jobTW] = useTypewriter({
-		words: ["<SoftwareEngineer />", "<FrontEndDev />"],
-		loop: 0,
+		words: ["<SoftwareEngineer />"],
+		loop: 1,
+    typeSpeed: 50,
 		delaySpeed: 2500,
 	})
+
+	const [caption, captionTW] = useTypewriter({
+		words: ["Let's Build Something Great Together", "I Use VIM BTW", "Let's Build Something Great Together", "I Use Arch BTW"],
+		loop: 0,
+    typeSpeed: 30,
+    deleteSpeed: 30,
+		delaySpeed: 5000,
+	})
+
 	return (
 		<div className='flex h-screen flex-col items-center justify-center space-y-8 overflow-hidden text-center'>
 			<BGCircle />
 			<div className='z-50'>
 				<h1 className='text-sm uppercase tracking-widest text-gray-600 dark:text-primary-white-3 sm:text-lg'>
-					Let&apos;s Build Something Great Together
+          {caption}
+					<Cursor />
 				</h1>
 				<motion.h1
 					initial='hidden'
@@ -40,9 +51,8 @@ export default function Main() {
 					className='bold py-2 text-gray-700 dark:text-primary-white-2'
 				>
 					<span className='text5xl scroll-px-10 text-accent lg:text-6xl'>
-						{job}
+          {job}
 					</span>
-					<Cursor />
 				</motion.h1>
 				<p className='m-auto max-w-[70%] py-4 text-gray-600 dark:text-primary-white-3'></p>
 				<div className='m-auto flex max-w-[330px] items-center justify-between py-4'>
