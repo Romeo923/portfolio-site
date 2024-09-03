6b53b3bae463933af68b931ec0e89de53cd4f1b2
Romeo923 <cromeo5112@gmail.com>
2024-09-03 00:35:29 -0400

    Updated About Me

--- components/About.tsx
+++ components/About.tsx
@@ -22,7 +22,7 @@ export default function About() {
 					variants={fadeIn({ direction: "right", delay: 0 })}
 					whileInView={"show"}
 					viewport={{ once: false, amount: 0.7 }}
-					className='my-4 px-4 py-2 ease-in '
+					className='my-4 px-4 pt-2 ease-in '
 				>
 					<h2 className='pb-5'>Who Am I?</h2>
 					<p className='text-gray-600 dark:text-primary-white-3'>
@@ -30,11 +30,10 @@ export default function About() {
             I&apos;m passionate about developing innovative solutions and optimizing complex systems.
 					</p>
 					<p className='text-gray-600 dark:text-primary-white-3'>
-            I have experience in C++ and excel at tackling challenging problems while continuing to hone my skills.
-            At General Dynamics Electric Boat, I contribute to the development of advanced ship control systems.
+            I have strong experience in C++ and excel at tackling challenging problems. I use every opertunity to learn and find ways to improve upons my skillset.
 					</p>
 					<p className='text-gray-600 dark:text-primary-white-3'>
-            Outside of my professional work, I enjoy building PCs and gaming.
+            Outside of my professional work, I enjoy learning about new tech, building PCs, and gaming.
 					</p>
 				</motion.div>
 				<motion.div
@@ -42,14 +41,54 @@ export default function About() {
 					variants={fadeIn({ direction: "right", delay: 0 })}
 					whileInView={"show"}
 					viewport={{ once: false, amount: 0.7 }}
-					className='m-auto px-4 py-2 ease-in '
+					className='my-4 px-4 ease-in '
 				>
-					<h2 className='pb-2 text-2xl'>
-						Upsilon Pi Epsilon Honors Society - Vice President
-					</h2>
-					<p className='text-gray-600 dark:text-primary-white-3'>
-            I served as Vice President of the University of Bridgeport Delta Chapter of Upsilon Pi Epsilon (UPE) Honors Society for 2 years. UPE is an international honors society dedicated to the computing and information disciplines, where I played a role in fostering academic excellence and community engagement.
-					</p>
+					<div className='grid grid-cols-2'>
+            <h2 className='pb-5 col-span-2'>Experience</h2>
+						<h3 className='text-gray-900 dark:text-primary-white-1'>
+							General Dynamics Electric Boat
+						</h3>
+						<p className='text-gray-600 dark:text-primary-white-3'>
+							August 2023 - Present
+						</p>
+						<p className='col-span-2 text-gray-600 dark:text-primary-white-3 md:col-span-1'>
+							Embedded Software Engineer
+						</p>
+          </div>
+				</motion.div>
+				<motion.div
+					initial='hidden'
+					variants={fadeIn({ direction: "right", delay: 0 })}
+					whileInView={"show"}
+					viewport={{ once: false, amount: 0.7 }}
+					className='my-4 px-4 ease-in '
+				>
+          <p className='text-gray-600 dark:text-primary-white-3'>
+            At GDEB, my role is to support efforts in designing, developing, and testing software for ship control systems.
+          </p>
+          <p className='text-gray-600 dark:text-primary-white-3'>
+            Within my first year, I was able to quickly integrate myself with the team, become aquainted with systems and procedures, and identify several areas of improvement and opportunities for automation.
+          </p>
+          <p className='text-gray-600 dark:text-primary-white-3'>
+            I recieved an Operational Excellence Award for completely overhauling the test team workflow by automating redundant tasks and drastically improving tools utilized by the test team.
+          </p>
+				</motion.div>
+				<motion.div
+					initial='hidden'
+					variants={fadeIn({ direction: "right", delay: 0 })}
+					whileInView={"show"}
+					viewport={{ once: false, amount: 0.7 }}
+					className='my-4 px-4 ease-in '
+				>
+          <p className='text-gray-600 dark:text-primary-white-3'>
+            In addition, I was tasked with assisting a project that was falling behind get back on schedule. I wrote a script to read our local database files and generate code for each subsystem based that data.
+          </p>
+          <p className='text-gray-600 dark:text-primary-white-3'>
+            This script reduced the number of people required for this task,  allowing members to dedicate more time to various other areas of the project.
+          </p>
+          <p className='text-gray-600 dark:text-primary-white-3'>
+            While working here, I've improved my skills in C++ and git as well as gained experience working in a professional team environment.
+          </p>
 				</motion.div>
 				<motion.div
 					initial='hidden'
@@ -59,21 +98,24 @@ export default function About() {
 					className='mx-auto px-4 py-2 ease-in'
 				>
 					<div className='grid grid-cols-2'>
-						<h2 className='col-span-2'>Education</h2>
-						<p className='text-gray-600 dark:text-primary-white-3'>
+						<h2 className='pb-5 col-span-2'>Education</h2>
+						<h3 className='text-gray-900 dark:text-primary-white-1'>
 							MS. Computer Science
-						</p>
+						</h3>
 						<p className='text-gray-600 dark:text-primary-white-3'>
 							University of Bridgeport
 						</p>
 						<p className='col-span-2 py-2 text-gray-600 dark:text-primary-white-3 md:col-span-1 md:col-start-2'>
 							May 2024
 						</p>
-						<h3 className='col-span-2 text-2xl'>
+						<h3 className='pb-2 col-span-2 text-2xl'>
 							Relavent Courses
 						</h3>
 						<p className='text-gray-600 dark:text-primary-white-3'>
 							Algorithms
+						</p>
+						<p className='text-gray-600 dark:text-primary-white-3'>
+							Cloud Computing
 						</p>
 						<p className='text-gray-600 dark:text-primary-white-3'>
 							OOP W/ Design Patterns
@@ -87,10 +129,21 @@ export default function About() {
 						<p className='text-gray-600 dark:text-primary-white-3'>
 							Data Mining
 						</p>
-						<p className='text-gray-600 dark:text-primary-white-3'>
-							Cloud Computing
-						</p>
 					</div>
+				</motion.div>
+				<motion.div
+					initial='hidden'
+					variants={fadeIn({ direction: "right", delay: 0 })}
+					whileInView={"show"}
+					viewport={{ once: false, amount: 0.7 }}
+					className='m-auto px-4 py-2 ease-in'
+				>
+					<h2 className='pb-2 text-2xl'>
+						Upsilon Pi Epsilon Honors Society - Vice President
+					</h2>
+					<p className='text-gray-600 dark:text-primary-white-3'>
+            I served as Vice President of the University of Bridgeport Delta Chapter of Upsilon Pi Epsilon (UPE) Honors Society for 2 years. UPE is an international honors society dedicated to the computing and information disciplines, where I played a role in fostering academic excellence and community engagement.
+					</p>
 				</motion.div>
 			</div>
 			<motion.div
